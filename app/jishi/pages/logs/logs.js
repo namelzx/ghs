4a171a716cd92d6adc61a0c6043b3918c7578d08
namelzx <914057262@qq.com>
2020-04-21 06:52:10 +0800

    更新第二版海报

--- app/jishi/pages/logs/logs.js
+++ app/jishi/pages/logs/logs.js
@@ -34,10 +34,11 @@ Page({
       console.log("宽：" + parseInt(myCanvasWidth / 5), "高:" + parseInt(myCanvasHeight / 7.88))
       // var avatarurl_width = parseInt (myCanvasWidth / 5);    //绘制的头像宽度
       //将方形图片处理成圆形头像
-      var avatarurl_heigth = parseInt(myCanvasHeight / 7);   //绘制的头像高度
+      var avatarurl_heigth = parseInt(myCanvasHeight / 10);   //绘制的头像高度
       var avatarurl_width = avatarurl_heigth
       var avatarurl_x = myCanvasWidth - (myCanvasWidth-20);   //绘制的头像在画布上的位置
-      var avatarurl_y = myCanvasHeight - myCanvasHeight * 0.16  //绘制的头像在画布上的位置
+      var avatarurl_y = myCanvasHeight - (myCanvasHeight*1) //绘制的头像在画布上的位置
+      console.log(myCanvasHeight,'3')
       that.setData({
         canvasWidth: myCanvasWidth,
         canvasHeight: myCanvasHeight,
@@ -56,9 +57,9 @@ Page({
       
       context.fillRect(0, 0, myCanvasWidth, myCanvasHeight)
 
-      context.drawImage('../../imgs/bg.png', 0, 0, myCanvasWidth, myCanvasHeight)
+      context.drawImage('../../imgs/goodsbg2.jpg', 0, 0, myCanvasWidth, myCanvasHeight)
       context.fill()
-      context.drawImage(that.data.images_url, 0, 0, myCanvasWidth , myCanvasHeight/1.8 )
+      context.drawImage(that.data.images_url, myCanvasWidth - (myCanvasWidth * 0.95), myCanvasHeight - (myCanvasHeight*0.85), myCanvasWidth/1.1 , myCanvasHeight/2.5 )
       context.fill()
 
 
@@ -72,7 +73,6 @@ Page({
       context.clip();//画好了圆 剪切  原始画布中剪切任意形状和尺寸。一旦剪切了某个区域，则所有之后的绘图都会被限制在被剪切的区域内 这也是我们要save上下文的原因
 
 
-      console.log(avatarurl_x, avatarurl_y)
       context.drawImage(that.data.avatarUrl, avatarurl_x, avatarurl_y, avatarurl_width/1, avatarurl_heigth); 
       context.restore(); //恢复之前保存的绘图上下文 恢复之前保存的绘图上下午即状态 还可以继续绘制
       context.setFontSize(14)
@@ -97,13 +97,10 @@ Page({
   
       context.setFillStyle('#333333')
       context.setFontSize(14)
-      context.fillText(that.data.userinfo.nickName + "正在分享商品", myCanvasWidth - (myCanvasWidth * 0.75), myCanvasHeight - (myCanvasHeight * 0.1))
-      context.setFillStyle('#999999')
-      context.setFontSize(12)
-      context.fillText("(长按识别,了解详情)", myCanvasWidth - (myCanvasWidth - (myCanvasWidth*0.25)), myCanvasHeight - (myCanvasHeight*0.07))
-
+      context.fillText(that.data.userinfo.nickName + "正在分享商品", myCanvasWidth - (myCanvasWidth * 0.82), myCanvasHeight - (myCanvasHeight * 0.95))
+     
     
-      context.drawImage(that.data.code, myCanvasWidth - 100, myCanvasHeight - (myCanvasHeight*0.13), myCanvasWidth / 6, myCanvasWidth/6); 
+      context.drawImage(that.data.code, myCanvasWidth - (myCanvasWidth*0.9), myCanvasHeight - (myCanvasHeight*0.22), myCanvasWidth / 4, myCanvasWidth/4); 
       context.draw()
 
       //定时器作用同上
